0aac79fa6f0cd1e8e057cc99df952e2722ee8088
artOops <sir.tema2014@yandex.ru>
2023-12-24 17:42:15 +0500

    fix check style

--- src/main/java/ru/practicum/shareit/exception/exeption/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/exception/exeption/NotFoundException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception.exeption;
 
-public class NotFoundException extends RuntimeException{
+public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
     }
